0436b9942d05e92cf4a6c1b3d94a09c7e7dfb36a
mfdivision <133631260+mfdivision@users.noreply.github.com>
2023-11-21 12:26:20 +0500

    Update tags.py

--- news/templatetags/tags.py
+++ news/templatetags/tags.py
@@ -1,14 +1,6 @@
-from datetime import datetime
-
 from django import template
 
-
 register = template.Library()
-
-
-@register.simple_tag()
-def current_time(format_string='%b %d %Y'):
-    return datetime.utcnow().strftime(format_string)
 
 
 @register.filter()
